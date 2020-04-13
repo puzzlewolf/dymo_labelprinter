652add6018104e6597e8380d85d6b866e42ce551
puzzlewolf <mail@nora.pink>
2020-04-13 16:40:25 +0200

    add shell.nix

---
+++ shell.nix
@@ -0,0 +1,20 @@
+with import <nixpkgs> {};
+stdenv.mkDerivation {
+  name = "rust-env";
+  buildInputs = [
+    # this includes, cargo, rustc, rls, rustfmt
+#    (rustChannelOf (parseRustToolchain ./rust-toolchain)).rust
+#    cargo-tree
+#    cargo-audit
+#    cargo-clippy
+    rustup
+  ];
+
+  nativeBuildInputs = with pkgs; [
+  #  llvmPackages.libclang
+  #  llvmPackages.clang
+  ]; 
+  # Set Environment Variables
+  RUST_BACKTRACE = 1;
+}
+

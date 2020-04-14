8b5ce7615f2ce0ea639c1a80ce4b4dadd16bcc72
puzzlewolf <mail@nora.pink>
2020-04-14 15:57:00 +0200

    restructure PrintabeImage

--- src/main.rs
+++ src/main.rs
@@ -10,66 +10,100 @@ struct PrintableImage {
     data: Vec<[u8; 8]>,
 }
 
-impl PrintableImage {
+struct CommandAccumulator {
+    accu: Vec<u8>,
+}
+
+impl CommandAccumulator {
     const SYN: u8 = 0x16;
     const ESC: u8 = 0x1b;
 
-    pub fn to_commands(&self) -> Vec<u8> {
-        let mut command_vec = PrintableImage::preamble();
-        command_vec.append(&mut PrintableImage::bytes_per_line(8));
-        for row in self.data.iter() {
-            PrintableImage::append_data_row(&mut command_vec, &row);
-        }
-        command_vec.append(&mut PrintableImage::bytes_per_line(0));
-        command_vec.append(&mut vec![PrintableImage::SYN; 56]);
-        command_vec.append(&mut vec![PrintableImage::SYN; 56]);
-        command_vec.append(&mut PrintableImage::get_status());
-        command_vec
+    //pub fn to_commands(&self) -> Vec<u8> {
+    //    let mut command_vec = PrintableImage::preamble();
+    //    command_vec.append(&mut PrintableImage::bytes_per_line(8));
+    //    for row in self.data.iter() {
+    //        PrintableImage::append_data_row(&mut command_vec, &row);
+    //    }
+    //    command_vec.append(&mut PrintableImage::bytes_per_line(0));
+    //    command_vec.append(&mut vec![PrintableImage::SYN; 56]);
+    //    command_vec.append(&mut vec![PrintableImage::SYN; 56]);
+    //    command_vec.append(&mut PrintableImage::get_status());
+    //    command_vec
+    //}
+
+    fn image_to_lines(image: &PrintableImage) {
+
     }
 
-    fn append_data_row(command_vec: &mut Vec<u8>, row: &[u8; 8]) {
-        command_vec.push(PrintableImage::SYN);
-        command_vec.extend(row);
+    /// Add the print commands for one row of the image.
+    /// Before the line, `bytes_per_line` must be set to the correct value.
+    fn append_data_row(&self, row: &[u8; 8]) {
+        self.accu.push(Self::SYN);
+        self.accu.extend(row);
     }
 
-    fn preamble() -> Vec<u8> {
-        let mut preamble = PrintableImage::tapecolor();
-        preamble.append(&mut PrintableImage::dottab());
-        preamble
+    fn preamble(&self, add_whitespace: bool) {
+        self.tape_color();
+        self.dottab();
+        if add_whitespace {
+            self.append_whitespace(56);
+        };
     }
 
-    fn print_commands(&self) {
-        let mut c = self.to_commands();
-        while c.len() > 8 {
-            let tmp = c.split_off(8);
-            println!("{:x?}", &c[0..8]);
-            //c.iter().for_each(|byte| print!("{:x?} ", byte));
-            c = tmp;
-        }
+    fn postamble(&self, add_whitespace: bool) {
+        if add_whitespace {
+            self.append_whitespace(56);
+        };
+        self.get_status();
+    }
+
+    //fn print_commands(&self) {
+    //    let mut c = self.to_commands();
+    //    while c.len() > 8 {
+    //        let tmp = c.split_off(8);
+    //        println!("{:x?}", &c[0..8]);
+    //        //c.iter().for_each(|byte| print!("{:x?} ", byte));
+    //        c = tmp;
+    //    }
+    //}
+
+    /// Add `num` lines of whitespace.
+    ///
+    /// 56 is recommended as space before and after the label text.
+    fn append_whitespace(&self, num: usize) {
+      self.bytes_per_line(0);
+      (0..num).for_each(|_| self.accu.push(Self::SYN));
     }
 
     /// The number of bytes in the following row(s).
     /// Seems to take no arguments.
-    fn get_status() -> Vec<u8> {
-        vec![PrintableImage::ESC, 'A' as u8]
+    fn get_status(&self) {
+        self.accu.push(Self::ESC);
+        self.accu.push('A' as u8);
     }
 
     /// The number of bytes in the following row(s).
     /// Seems to take one byte argument.
-    fn bytes_per_line(num: u8) -> Vec<u8> {
-        vec![PrintableImage::ESC, 'D' as u8, num]
+    fn bytes_per_line(&self, num: u8) {
+        self.accu.push(Self::ESC);
+        self.accu.push('D' as u8);
+        self.accu.push(num);
     }
 
     /// The tape's color. Encoding unknown.
     /// Seems to take one byte argument.
-    fn tapecolor() -> Vec<u8> {
-        vec![PrintableImage::ESC, 'C' as u8, 0]
+    fn tape_color(&self) {
+        self.accu.push(Self::ESC);
+        self.accu.push('C' as u8);
+        self.accu.push(0 as u8);
     }
 
     /// Probably whether (or how?) to print the tab character.
     /// Seems to take one byte argument.
-    fn dottab() -> Vec<u8> {
-        vec![PrintableImage::ESC, 'B' as u8, 0]
+    fn dottab(&self) {
+        self.accu.push(Self::ESC);
+        self.accu.push('B' as u8);
+        self.accu.push(0 as u8);
     }
 
 // constants:
@@ -80,7 +114,6 @@ impl PrintableImage {
 //      D bytesperline, one argument, used as ESC, B, num_of_bytes e.g. 1b 44 07
 //      C tapecolour, one argument, 0 known used
 //      B dottab, one argument, 0 known used
-
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> { 
@@ -169,39 +202,44 @@ impl ColorMap for DynamicBiLevel {
 
 #[test]
 fn test_append_row() {
-  let mut command: Vec<u8> = vec![17u8; 7];
-  PrintableImage::append_data_row(&mut command, &mut [0u8, 1u8, 2u8, 3u8, 4u8, 5u8,6u8, 7u8]);
-  assert_eq!(command[0..7], [17u8; 7]);
-  assert_eq!(command[7], 0x16); 
-  assert_eq!(command[8..16], [0,1,2,3,4,5,6,7]);
+  let ca = CommandAccumulator{accu: Vec::new()};
+  ca.append_data_row(&mut [0u8, 1u8, 2u8, 3u8, 4u8, 5u8,6u8, 7u8]);
+  assert_eq!(ca.accu[0..7], [17u8; 7]);
+  assert_eq!(ca.accu[7], 0x16); 
+  assert_eq!(ca.accu[8..16], [0,1,2,3,4,5,6,7]);
 }
 
 #[test]
 fn test_preamble() {
-  let preamble = PrintableImage::preamble();
-  assert_eq!(preamble[0..6], [0x1b, 0x43, 0, 0x1b, 0x44, 0]);
+  let ca = CommandAccumulator{accu: Vec::new()};
+  ca.preamble(false);
+  assert_eq!(ca.accu[0..6], [0x1b, 0x43, 0, 0x1b, 0x44, 0]);
 }
 
 #[test]
 fn test_get_status() {
-  let get_status = PrintableImage::get_status();
-  assert_eq!(get_status[0..2], [0x1b, 0x41]);
+  let ca = CommandAccumulator{accu: Vec::new()};
+  ca.get_status();
+  assert_eq!(ca.accu[0..2], [0x1b, 0x41]);
 }
 
 #[test]
 fn test_bytes_per_line() {
-  let bytes_per_line = PrintableImage::bytes_per_line(8);
-  assert_eq!(bytes_per_line[0..3], [0x1b, 0x42, 0x08]);
+  let ca = CommandAccumulator{accu: Vec::new()};
+  ca.bytes_per_line(8);
+  assert_eq!(ca.accu[0..3], [0x1b, 0x42, 0x08]);
 }
 
 #[test]
-fn test_tapecolor() {
-  let tapecolor = PrintableImage::tapecolor();
-  assert_eq!(tapecolor[0..3], [0x1b, 0x43, 0]);
+fn test_tape_color() {
+  let ca = CommandAccumulator{accu: Vec::new()};
+  ca.tape_color();
+  assert_eq!(ca.accu[0..3], [0x1b, 0x43, 0]);
 }
 
 #[test]
 fn test_dottab() {
-  let dottab = PrintableImage::dottab();
-  assert_eq!(dottab[0..3], [0x1b, 0x44, 0]);
+  let ca = CommandAccumulator{accu: Vec::new()};
+  ca.dottab();
+  assert_eq!(ca.accu[0..3], [0x1b, 0x44, 0]);
 }

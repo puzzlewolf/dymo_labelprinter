c79222bc04fcf35caaaa5a14327f0e164942a0bc
puzzlewolf <mail@nora.pink>
2020-04-18 15:38:04 +0200

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 use image::imageops::colorops::ColorMap;
 use image::*;
 
-use bitvec::prelude::*;
-
 use dymo_hid::color_map::DynamicBiLevel;
 use dymo_hid::command_accumulator::CommandAccumulator;
 use dymo_hid::printable_image::PrintableImage;
@@ -25,14 +23,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
     println!("Dimensions of image: {:?}", pic.dimensions());
     let pic = pic.rotate90();
-    let new_pic = convert_to_bw(&pic).unwrap();
-    new_pic.save("output/output.png")?;
-    print_file(&new_pic)?;
+    let new_pic = convert_to_bw(&pic, 128);
+    new_pic.save("output/preview.png")?;
+    write_commands(&new_pic)?;
     Ok(())
 }
 
-fn print_file(image: &GrayImage) -> Result<(), Box<dyn std::error::Error>> {
-    let pi = grey_to_printable(image)?;
+fn write_commands(image: &GrayImage) -> Result<(), Box<dyn std::error::Error>> {
+    let pi = PrintableImage::printable_from_grey(image)?;
     print!("{}", pi.preview());
     let mut ca = CommandAccumulator::new();
     ca.generate_commands(&pi);
@@ -42,39 +40,15 @@ fn print_file(image: &GrayImage) -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-fn convert_to_bw(image: &DynamicImage) -> ImageResult<GrayImage> {
+fn convert_to_bw(image: &DynamicImage, threshold: u8) -> GrayImage {
     let mut gray_image = image.grayscale().into_luma();
     gray_image
         .pixels_mut()
-        .for_each(|pix| px_to_black_or_white(pix));
-    Ok(gray_image)
+        .for_each(|pix| px_to_black_or_white(pix, threshold));
+    gray_image
 }
 
-fn px_to_black_or_white(pix: &mut Luma<u8>) {
-    let colormap = DynamicBiLevel { threshold: 128 };
+fn px_to_black_or_white(pix: &mut Luma<u8>, threshold: u8) {
+    let colormap = DynamicBiLevel { threshold };
     colormap.map_color(pix);
-}
-
-fn grey_to_printable(image: &GrayImage) -> Result<PrintableImage, Box<dyn std::error::Error>> {
-    let rows = image
-        .rows()
-        .map(|row| row_to_bitvec(row))
-        .collect::<Result<Vec<[u8; 8]>, _>>()?;
-    //println!("{:?}", bitvecs);
-    Ok(PrintableImage { data: rows })
-}
-
-fn row_to_bitvec(
-    row: image::buffer::Pixels<Luma<u8>>,
-) -> Result<[u8; 8], Box<dyn std::error::Error>> {
-    let bitvec: BitVec<Msb0, u8> = row.map(|pix| !is_pixel_white(pix)).collect();
-    let bytevec = &bitvec.into_vec();
-    let mut result = [0 as u8; 8];
-    let bytes = &bytevec.as_slice()[..result.len()]; // panics if not enough data
-    result.copy_from_slice(bytes);
-    Ok(result)
-}
-
-fn is_pixel_white(pixel: &Luma<u8>) -> bool {
-    pixel.to_luma()[0] == 0xFF
 }
--- src/printable_image.rs
+++ src/printable_image.rs
@@ -1,3 +1,6 @@
+use bitvec::prelude::*;
+use image::*;
+
 /// represents an image that can be printed on a dymo label printer.
 pub struct PrintableImage {
     // TODO private
@@ -15,4 +18,28 @@ impl PrintableImage {
         });
         output.replace("0", ".").replace("1", "X")
     }
+
+    pub fn printable_from_grey(image: &GrayImage) -> Result<Self, Box<dyn std::error::Error>> {
+        let rows = image
+            .rows()
+            .map(|row| row_to_bitvec(row))
+            .collect::<Result<Vec<[u8; 8]>, _>>()?;
+        //println!("{:?}", bitvecs);
+        Ok(PrintableImage { data: rows })
+    }
+}
+
+fn row_to_bitvec(
+    row: image::buffer::Pixels<Luma<u8>>,
+    ) -> Result<[u8; 8], Box<dyn std::error::Error>> {
+    let bitvec: BitVec<Msb0, u8> = row.map(|pix| !is_pixel_white(pix)).collect();
+    let bytevec = &bitvec.into_vec();
+    let mut result = [0 as u8; 8];
+    let bytes = &bytevec.as_slice()[..result.len()]; // panics if not enough data
+    result.copy_from_slice(bytes);
+    Ok(result)
+}
+
+fn is_pixel_white(pixel: &Luma<u8>) -> bool {
+    pixel.to_luma()[0] == 0xFF
 }

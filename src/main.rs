ac85ed298692ec4d427e1034278820e5559f5851
puzzlewolf <mail@nora.pink>
2020-04-19 11:11:22 +0200

    structopt and create image with imagemagick

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,26 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::io::{Error, ErrorKind};
 
+//use std::path::PathBuf;
+use structopt::StructOpt;
+
+pub mod opt;
+use opt::Opt;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let pic = image::open("testdata/bold.png")?;
-    let threshold = 128u8;
+    let opt = Opt::from_args();
+    println!("{:?}", opt);
+    let threshold = opt.threshold;
+
+    let pic = {
+        if let Some(image_arg) = opt.image {
+            image::open(image_arg)?
+        } else
+        {
+            create_image("testText")?
+        }
+    };
+    println!("Dimensions of image: {:?}", pic.dimensions());
 
     if pic.dimensions().1 != 64 {
         let errortext = format!(
@@ -21,7 +38,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         let errortext = format!("image must not have transparency!");
         return Err(Error::new(ErrorKind::Other, errortext))?;
     }
-    println!("Dimensions of image: {:?}", pic.dimensions());
 
     let pic = pic.rotate90();
     let bw_pic = convert_to_bw(&pic, threshold);
@@ -29,6 +45,27 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     write_commands(&bw_pic)?;
     Ok(())
+}
+
+fn create_image(text: &str) -> Result<DynamicImage, Box<dyn std::error::Error>> {
+    use std::process::Command;
+    let output = Command::new("convert")
+        .arg("-background")
+        .arg("white")
+        .arg("-fill")
+        .arg("black")
+        .arg("-font")
+        .arg("Ubuntu")
+        .arg("-gravity")
+        .arg("center")
+        .arg("-size")
+        .arg("x64")
+        .arg(["label:", text].join(""))
+        .arg("png:-") //output png image to stdout
+        .output()
+        .expect("failed to execute process");
+    let image = image::load_from_memory(&output.stdout)?;
+    Ok(image)
 }
 
 fn write_commands(image: &GrayImage) -> Result<(), Box<dyn std::error::Error>> {

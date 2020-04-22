f70a4a87b12592333d41b068ea0ce6b549e024f0
puzzlewolf <mail@nora.pink>
2020-04-22 12:04:08 +0200

    setup for image form

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -9,7 +9,7 @@ use dymo_label::picture;
 extern crate log;
 
 #[derive(Deserialize, Debug)]
-struct FormData {
+struct TextFormData {
     label_text: String,
 }
 
@@ -17,8 +17,8 @@ struct FormData {
 async fn preview_image(param: web::Path<String>) -> impl Responder {
     let result = handle_preview_text(param.to_string());
     match result {
-        Ok(image) => HttpResponse::Ok().content_type("image/png").body(image),
-        Err(err) => HttpResponse::InternalServerError().body(err.to_string()),
+        Ok(img) => HttpResponse::Ok().content_type("image/png").body(img),
+        Err(err) => error_response(err),
     }
 }
 
@@ -26,28 +26,35 @@ async fn preview_image(param: web::Path<String>) -> impl Responder {
 async fn preview_text(param: web::Path<String>) -> impl Responder {
     let result = handle_preview_text(param.to_string());
     match result {
-        Ok(image) => HttpResponse::Ok().content_type("image/png").body(image),
-        Err(err) => HttpResponse::InternalServerError().body(err.to_string()),
+        Ok(img) => HttpResponse::Ok().content_type("image/png").body(img),
+        Err(err) => error_response(err),
     }
 }
 
 #[post("/print/text")]
-async fn print_text(form: web::Form<FormData>) -> impl Responder {
+async fn print_text(form: web::Form<TextFormData>) -> impl Responder {
     debug!("Form Data: {:?}!", form);
     let result = handle_print_text(&form.label_text);
     match result {
         Ok(_) => HttpResponse::Ok()
             .content_type("text/plain; charset=utf-8")
             .body("Go get your label!"),
-        Err(err) => HttpResponse::InternalServerError().body(err.to_string()),
+        Err(err) => error_response(err),
     }
 }
 
-#[get("/")]
-async fn index() -> impl Responder {
+#[get("/text")]
+async fn text() -> impl Responder {
     HttpResponse::Ok()
         .content_type("text/html; charset=utf-8")
-        .body(include_str!("../static/index.html"))
+        .body(include_str!("../static/text.html"))
+}
+
+#[get("/image")]
+async fn image() -> impl Responder {
+    HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(include_str!("../static/image.html"))
 }
 
 #[get("/static/site.css")]
@@ -60,17 +67,22 @@ async fn css() -> impl Responder {
 fn handle_print_text(label_text: &str) -> Result<(), Box<dyn std::error::Error>> {
     info!("label text: {}", label_text);
     let pic = picture::create_image(&label_text, "Ubuntu")?;
-
     let bw_pic = picture::convert_to_bw(&pic, 128)?;
+
     dymo_label::print_label(&bw_pic)
 }
 
 fn handle_preview_text(label_text: String) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
     info!("label text: {}", label_text);
     let pic = picture::create_image(&label_text, "Ubuntu")?;
-
     let bw_pic = picture::convert_to_bw(&pic, 128)?;
+
     picture::encode_png(&bw_pic)
+}
+
+fn error_response(err: Box<dyn std::error::Error>) -> HttpResponse {
+    error!("{}", err);
+    HttpResponse::InternalServerError().body(err.to_string())
 }
 
 #[actix_rt::main]
@@ -81,8 +93,9 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .wrap(Logger::default())
             .wrap(Logger::new("%a %{User-Agent}i"))
-            .service(index)
             .service(css)
+            .service(text)
+            .service(image)
             .service(preview_text)
             .service(print_text)
     })

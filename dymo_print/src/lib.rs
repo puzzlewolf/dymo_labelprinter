efbfb87753b97d9a581f1066ae442f212d72335b
puzzlewolf <mail@nora.pink>
2020-05-01 17:28:29 +0200

    Minor cleanup.

--- dymo_print/src/lib.rs
+++ dymo_print/src/lib.rs
@@ -9,7 +9,7 @@ use crate::command_accumulator::CommandAccumulator;
 use std::fs::File;
 use std::io::prelude::*;
 
-static IM_CONVERT: &'static str = env!("IM_CONVERT");
+static IM_CONVERT: &str = env!("IM_CONVERT");
 
 pub fn print_label(image: &image::GrayImage) -> Result<(), Box<dyn std::error::Error>> {
     let pi = crate::picture::PrintableImage::printable_from_grey(image)?;

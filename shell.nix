ead1ba97df6bd2c4aaff6c00998f5318719e0255
puzzlewolf <mail@nora.pink>
2021-08-15 16:55:31 +0200

    use oxalica overlay in shell.nix

--- shell.nix
+++ shell.nix
@@ -1,20 +1,33 @@
-with import <nixpkgs> {};
-stdenv.mkDerivation {
-  name = "rust-env";
+let
+  oxalica_overlay = import (builtins.fetchTarball
+    "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
+  nixpkgs = import <nixpkgs> {
+    overlays =
+      [ oxalica_overlay (import ~/.config/nixpkgs/overlays/overlays.nix) ];
+  };
+  #rust_channel = nixpkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain;
+  rust_channel = nixpkgs.rust-bin.stable."1.52.0".default;
+
+  #moz_overlay = import (builtins.fetchTarball
+  #  "https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz");
+  #nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+  #rust_channel = (nixpkgs.rustChannelOf { rustToolchain = ./rust-toolchain; }).rust;
+in with nixpkgs;
+pkgs.mkShell {
   buildInputs = [
-    # this includes, cargo, rustc, rls, rustfmt
-#    (rustChannelOf (parseRustToolchain ./rust-toolchain)).rust
-#    cargo-tree
-#    cargo-audit
-#    cargo-clippy
-    rustup
+
+    #    cargo-audit
     cargo-outdated
   ];
 
-  nativeBuildInputs = with pkgs; [
-  #  llvmPackages.libclang
-  #  llvmPackages.clang
-  ];
+  nativeBuildInputs = with pkgs;
+    [
+      (rust_channel.override {
+        extensions = [ "rust-src" "rust-std" "clippy-preview" ];
+      })
+      #  llvmPackages.libclang
+      #  llvmPackages.clang
+    ];
   # Set Environment Variables
   RUST_BACKTRACE = 1;
   IM_CONVERT = "${pkgs.imagemagick}/bin/convert";
